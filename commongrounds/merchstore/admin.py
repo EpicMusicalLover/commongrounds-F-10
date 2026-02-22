9597ca8dcfcc2345373b66d0614b91758b109de3
Nathaniel Joaquin Quijencio <natesjoaquin@gmail.com>
2026-02-22 20:55:02 +0800

    added the admin panels for ProductType and Product with both panels having list_display, search)field, and ordering and an additional list_filter for the Product admin panel

--- commongrounds/merchstore/admin.py
+++ commongrounds/merchstore/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import ProductType, Product
 
-# Register your models here.
+class ProductTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name', )
+    ordering = ('name', )
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'product_type', 'price')
+    list_filter = ('product_type', )
+    search_fields = ('name', 'description')
+    ordering = ('name', )
+
+admin.site.register(ProductType, ProductTypeAdmin)
+admin.site.register(Product, ProductAdmin)
