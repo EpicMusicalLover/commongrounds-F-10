b295e956cf924b151897a95e1d14317fb5fd43ce
Nathaniel Joaquin Quijencio <natesjoaquin@gmail.com>
2026-02-22 21:11:16 +0800

    reformatted more files according to pep8

--- commongrounds/merchstore/migrations/0001_initial.py
+++ commongrounds/merchstore/migrations/0001_initial.py
@@ -44,7 +44,12 @@ class Migration(migrations.Migration):
                 ),
                 ("name", models.CharField(max_length=255)),
                 ("description", models.TextField()),
-                ("price", models.DecimalField(decimal_places=2, max_digits=10)),
+                (
+                    "price", models.DecimalField(
+                        decimal_places=2,
+                        max_digits=10
+                    )
+                ),
                 (
                     "product_type",
                     models.ForeignKey(
