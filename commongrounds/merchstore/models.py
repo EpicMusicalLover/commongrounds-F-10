68bb501cc687ef20cc33a9b630155a48d19b4197
Nathaniel Joaquin Quijencio <natesjoaquin@gmail.com>
2026-02-22 19:36:27 +0800

    added the ProductType and Product models in models.py and added the product list and detail views in views.py

--- commongrounds/merchstore/models.py
+++ commongrounds/merchstore/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.urls import reverse
+
+class ProductType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
 
 # Create your models here.
--- commongrounds/merchstore/views.py
+++ commongrounds/merchstore/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+from .models import Product
+
+
+class RecipeListView(ListView):
+    model = Product
+    template_name = "pending.html"
+
+
+class RecipeDetailView(DetailView):
+    model = Product
+    template_name = "pending.html"
+
 
 # Create your views here.
