154501532045bfbb02b6540a1fd15240d3e645ac
annapaulaaranas-stack <anna.paula.aranas@student.ateneo.edu>
2026-02-28 22:19:53 +0800

    added the book list and detail view in views.py

--- commongrounds/bookclub/views.py
+++ commongrounds/bookclub/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Book
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+
+class BookListView(ListView):
+    model = Book
+    template_name = "book_list.html"
+
+
+class BookDetailView(DetailView):
+    model = Book
+    template_name = "book_detail.html"
