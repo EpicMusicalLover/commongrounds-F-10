cd6d76a709537316f5fc46e589a33958efb71421
annapaulaaranas-stack <anna.paula.aranas@student.ateneo.edu>
2026-03-01 15:49:40 +0800

    added the admin panels for Genre and Book

--- commongrounds/bookclub/admin.py
+++ commongrounds/bookclub/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Genre, Book
 
-# Register your models here.
+
+class GenreAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+    list_display = ("name",)
+    ordering = ("name",)
+
+
+class BookAdmin(admin.ModelAdmin):
+    search_fields = ("title", "author",)
+    list_display = ("title", "author", "publication_year", "genre",)
+    list_filter = ("genre", "publication_year",)
+    ordering = ("-publication_year")
+
+
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Book, BookAdmin)
