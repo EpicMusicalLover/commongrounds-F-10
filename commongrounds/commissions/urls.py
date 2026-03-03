d4c13976c4cea0d90796cd73f057045c22eee6bd
Kandice <april.kandice.pauline.cuizon@student.ateneo.edu>
2026-03-03 11:52:13 +0800

    Added appropriate URLs to urls.py in commissions and commongrounds

--- commongrounds/commissions/urls.py
+++ commongrounds/commissions/urls.py
@@ -1,1 +1,9 @@
 from django.urls import path
+from .views import CommissionListView, CommissionDetailView
+
+
+app_name = "commissions"
+urlpatterns = [
+    path("requests/", CommissionListView.as_view(), name="commission-list"),
+    path("request/<int:pk>/", CommissionDetailView.as_view(), name="commission-detail"),
+]
