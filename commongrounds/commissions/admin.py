4bd96579ffc2ab5a41a9f80aa2a38d4103535ac2
Kandice <april.kandice.pauline.cuizon@student.ateneo.edu>
2026-03-03 12:56:12 +0800

    Added admin panels for each model, changed timezone to local

--- commongrounds/commissions/admin.py
+++ commongrounds/commissions/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import CommissionType, Commission
 
-# Register your models here.
+
+class CommissionTypeAdmin(admin.ModelAdmin):
+    list_display = ("name", "description",)
+    search_fields = ("name",)
+    ordering = ("name",)
+
+
+class CommissionAdmin(admin.ModelAdmin):
+    list_display = ("title", "description", "people_required",)
+    search_fields = ("title", "description",)
+    ordering = ("created_on",)
+
+
+admin.site.register(CommissionType, CommissionTypeAdmin)
+admin.site.register(Commission, CommissionAdmin)
