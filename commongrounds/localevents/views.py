1240a894c4fce9e6df1296ddfe5025cea388cf2f
Andre Reyes <andrejeremyreyes@gmail.com>
2026-03-05 22:21:51 +0800

    added eventlistview and eventdetailview in views.py

--- commongrounds/localevents/views.py
+++ commongrounds/localevents/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+from .models import Event
 
-# Create your views here.
+class EventListView(ListView):
+    model = Event
+    template_name = "event_list.html"
+
+
+class EventDetailView(DetailView):
+    model = Event
+    template_name = "event_detail.html"
